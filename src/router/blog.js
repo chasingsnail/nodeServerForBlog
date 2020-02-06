4e03371e7654c82d9a989f8b7bba484ecbd7acd1
王杨芃 <wangyangpeng@nongfenqi.com>
2020-02-06 09:35:21 +0800

    feat(blogs): 加入登录校验

--- src/router/blog.js
+++ src/router/blog.js
@@ -7,6 +7,15 @@ const {
 } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+// 登录验证
+const loginCheck = req => {
+	if (!req.session.username) {
+		return Promise.resolve(
+			new ErrorModel('尚未登录')
+		)
+	}
+}
+
 const handleBlogRouter = async(req, res) => {
   const method = req.method
   const id = req.query.id
@@ -29,20 +38,32 @@ const handleBlogRouter = async(req, res) => {
 
 	// 新增博客
 	if (method === 'POST' && req.path === '/api/blog/new') {
-		req.body.author = 'lisi'
+		const loginResult = loginCheck(req)
+		if (loginResult) {
+			return loginResult
+		}
+		req.body.author = req.session.username
 		const data = await createBlog(req.body)
 		return new SuccessModel(data, '新增成功')
   }
   
   // 更新博客
   if (method === 'POST' && req.path === '/api/blog/update') {
+		const loginResult = loginCheck(req)
+		if (loginResult) {
+			return loginResult
+		}
 		const flag = await updateBlog(id, req.body)
 		return flag ? new SuccessModel('修改成功') : new ErrorModel('失败')
   }
 
   // 删除博客
   if (method === 'POST' && req.path === '/api/blog/del') {
-		const flag = await delBlog(id, 'lisi')
+		const loginResult = loginCheck(req)
+		if (loginResult) {
+			return loginResult
+		}
+		const flag = await delBlog(id, req.session.username)
 		return flag ? new SuccessModel('删除成功') : new ErrorModel('失败')
   }
 }
