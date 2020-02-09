ea653c17418dca621fd7623588ff4c8e833a147c
chasingsnail <wyp402905456@hotmail.com>
2020-02-09 10:15:45 +0800

    feat(sql): 预防sql注入

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,8 +1,10 @@
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
 
 const login = async(data) => {
   const { username, password } = data
-  const sql = `select username, realname from users where username='${username}' and password='${password}'`
+  const _username = escape(username)
+  const _pwd = escape(password)
+  const sql = `select username, realname from users where username=${_username} and password=${_pwd}`
   const res = await exec(sql)
   return res[0] || {}
 }
